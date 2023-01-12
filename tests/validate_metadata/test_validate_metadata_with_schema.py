b32dda675718a51303952c382fcfa194b96d2734
gdmiron <mirondanro@yahoo.com>
2023-01-12 17:42:30 +0100

    test to validate sensor metadata

---
+++ tests/validate_metadata/test_validate_metadata_with_schema.py
@@ -0,0 +1,23 @@
+import pytest
+from pytest_json_schema import assert_valid_schema
+
+def test_validate_metadata():
+    # Define the data to validate
+    metadata = {
+    "sensor_id": "sensor-001",
+    "package_id": "package-001",
+    "position": {
+        "location": [0, 1, 0]
+    },
+    "measuring_quantity" :       {
+        "type": "temperature"
+      #        "unit": "K"
+      },
+    "independent_variable":       {
+        "type": "time",
+        "unit": "s"
+      }
+    }
+
+    with assert_valid_schema({'response': '/../../metadata/schemas/sensor.json'}):
+        response = metadata
