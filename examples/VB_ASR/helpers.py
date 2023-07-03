64e0ae7ab131d60ea20da6015151c74dad49e4dc
ajafari <ajafari@VDI0039.zit.bam.de>
2023-07-03 14:42:17 +0200

    Format codes with black

--- examples/VB_ASR/helpers.py
+++ examples/VB_ASR/helpers.py
@@ -5,33 +5,59 @@ import os
 
 try:
     import seaborn as sns
-    sns.set_theme(style="darkgrid") # style must be one of: white, dark, whitegrid, darkgrid, ticks
+
+    sns.set_theme(
+        style="darkgrid"
+    )  # style must be one of: white, dark, whitegrid, darkgrid, ticks
 except ModuleNotFoundError:
-    print(f"\n\n\t{'-' * 70}\n\tWARNING: It is recommended to install 'seaborn' to get nicer plots.\n\t{'-' * 70}\n\n")
+    print(
+        f"\n\n\t{'-' * 70}\n\tWARNING: It is recommended to install 'seaborn' to get nicer plots.\n\t{'-' * 70}\n\n"
+    )
+
 
 def make_path(_path):
     if not os.path.exists(_path):
         os.makedirs(_path)
 
-def plot_pdfs(dists, labels, colors=None, interval_prob=0.99, target=None \
-              , tit='', xl='', yl='pdf', _path='', _name='pdf plot', _format='.png', dpi=300, sz=16, _show_values=True):
+
+def plot_pdfs(
+    dists,
+    labels,
+    colors=None,
+    interval_prob=0.99,
+    target=None,
+    tit="",
+    xl="",
+    yl="pdf",
+    _path="",
+    _name="pdf plot",
+    _format=".png",
+    dpi=300,
+    sz=16,
+    _show_values=True,
+):
     """
     "dists" is a list of scipy.stats distributions
     "interval_prob" is the cumulative distribution (central) based on which the plot's interval is set.
     """
     from matplotlib import rc
-    rc('text', usetex=True)
-    rc('font', size=sz)
-    rc('legend', fontsize=sz)
-    rc('text.latex', preamble=r'\usepackage{cmbright}')
-    _name = _name.replace('$', '') # in case of latex formatted name
+
+    rc("text", usetex=True)
+    rc("font", size=sz)
+    rc("legend", fontsize=sz)
+    rc("text.latex", preamble=r"\usepackage{cmbright}")
+    _name = _name.replace("$", "")  # in case of latex formatted name
     if tit is None:
-        tit = 'PDF'
-    tit = tit.replace('_', '\_') # suitable for latex formatted title
-    xl = xl.replace('_', '\_') # suitable for latex formatted title
-    yl = yl.replace('_', '\_') # suitable for latex formatted title
-    medians = []; ms = []; stds = []; xxs = []
-    x_min = 1e20; x_max = -1e20
+        tit = "PDF"
+    tit = tit.replace("_", "\_")  # suitable for latex formatted title
+    xl = xl.replace("_", "\_")  # suitable for latex formatted title
+    yl = yl.replace("_", "\_")  # suitable for latex formatted title
+    medians = []
+    ms = []
+    stds = []
+    xxs = []
+    x_min = 1e20
+    x_max = -1e20
     for d in dists:
         medians.append(d.median())
         ms.append(d.mean())
@@ -41,60 +67,105 @@ def plot_pdfs(dists, labels, colors=None, interval_prob=0.99, target=None \
         x_min = min(x_min, xd0)
         x_max = max(x_max, xd1)
     from math import isnan
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
-    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
+    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter("%.1e"))
+    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter("%.2e"))
     max_pdf = 0.0
     max_std = 0.0
     if colors is None:
-        plt_options = len(dists) * [{'color':'black'}]
+        plt_options = len(dists) * [{"color": "black"}]
     else:
-        plt_options = [{'color':cl} for cl in colors]
+        plt_options = [{"color": cl} for cl in colors]
     for d, dist in enumerate(dists):
         if isnan(stds[d]):
-            print('WARNING: No pdf-plot for a distribution with an infinite standard deviation !')
+            print(
+                "WARNING: No pdf-plot for a distribution with an infinite standard deviation !"
+            )
         else:
             m_ = np.format_float_scientific(ms[d], unique=False, precision=2)
             median_ = np.format_float_scientific(medians[d], unique=False, precision=2)
             std_ = np.format_float_scientific(stds[d], unique=False, precision=2)
             pdf_ = dist.pdf(xxs[d])
             if _show_values:
-                label_ = labels[d] + r": $\begin{array}{rcl} \mbox{median} & \mbox{=} & " + str(median_) + " \\\\ \mbox{mean} & \mbox{=} & " + str(m_) + " \\\\ \mbox{std} & \mbox{=} & " + str(std_) + " \end{array} $"
+                label_ = (
+                    labels[d]
+                    + r": $\begin{array}{rcl} \mbox{median} & \mbox{=} & "
+                    + str(median_)
+                    + " \\\\ \mbox{mean} & \mbox{=} & "
+                    + str(m_)
+                    + " \\\\ \mbox{std} & \mbox{=} & "
+                    + str(std_)
+                    + " \end{array} $"
+                )
             else:
                 label_ = labels[d]
-            plt.plot(xxs[d], pdf_, label=label_, **plt_options[d]);
-            t_min = 1e20; t_max = -1e20
+            plt.plot(xxs[d], pdf_, label=label_, **plt_options[d])
+            t_min = 1e20
+            t_max = -1e20
             max_pdf = max(np.max(pdf_), max_pdf)
             max_std = max(stds[d], max_std)
     if target is not None:
-        if type(target)==list or type(target)==np.ndarray:
+        if type(target) == list or type(target) == np.ndarray:
             for i, t in enumerate(target):
                 target_ = np.format_float_scientific(t, unique=False, precision=2)
-                plt.bar(x=t, height=1.0*max_pdf, width=max_std*0.02, color=PltOptions['perfect']['color'])
+                plt.bar(
+                    x=t,
+                    height=1.0 * max_pdf,
+                    width=max_std * 0.02,
+                    color=PltOptions["perfect"]["color"],
+                )
                 if _show_values:
-                    plt.text(t, 0.9*i*max_pdf/(len(target)-1), f"target{i}=\n{target_}", ha='center', va='bottom', fontsize=sz)
+                    plt.text(
+                        t,
+                        0.9 * i * max_pdf / (len(target) - 1),
+                        f"target{i}=\n{target_}",
+                        ha="center",
+                        va="bottom",
+                        fontsize=sz,
+                    )
                 else:
-                    plt.text(t, 0.9*i*max_pdf/(len(target)-1), f"target{i}", ha='center', va='bottom', fontsize=sz)
+                    plt.text(
+                        t,
+                        0.9 * i * max_pdf / (len(target) - 1),
+                        f"target{i}",
+                        ha="center",
+                        va="bottom",
+                        fontsize=sz,
+                    )
             t_min, t_max = min(target), max(target)
         else:
             target_ = np.format_float_scientific(target, unique=False, precision=2)
-            plt.bar(x=target, height=1.0*max_pdf, width=max_std*0.03, color=PltOptions['perfect']['color'])
+            plt.bar(
+                x=target,
+                height=1.0 * max_pdf,
+                width=max_std * 0.03,
+                color=PltOptions["perfect"]["color"],
+            )
             if _show_values:
-                plt.text(target, 0, f"target={target_}", ha='center', va='bottom', fontsize=sz)
+                plt.text(
+                    target,
+                    0,
+                    f"target={target_}",
+                    ha="center",
+                    va="bottom",
+                    fontsize=sz,
+                )
             else:
-                plt.text(target, 0, f"target", ha='center', va='bottom', fontsize=sz)
+                plt.text(target, 0, f"target", ha="center", va="bottom", fontsize=sz)
             t_min, t_max = target, target
-    plt.xticks(np.linspace(min(x_min, t_min), max(x_max, t_max),3), fontsize=sz);
-    plt.yticks(np.linspace(0.0, max_pdf, 3), fontsize=sz);
+    plt.xticks(np.linspace(min(x_min, t_min), max(x_max, t_max), 3), fontsize=sz)
+    plt.yticks(np.linspace(0.0, max_pdf, 3), fontsize=sz)
     plt.title(tit, fontsize=sz)
     plt.xlabel(xl, fontsize=sz)
     plt.ylabel(yl, fontsize=sz)
-    plt.legend(fontsize=int(sz*0.7), loc='best')
+    plt.legend(fontsize=int(sz * 0.7), loc="best")
     plt.tight_layout()
-    plt.savefig(_path + _name + _format, bbox_inches='tight', dpi=dpi)
+    plt.savefig(_path + _name + _format, bbox_inches="tight", dpi=dpi)
     plt.show(block=False)
     plt.rcParams["text.usetex"] = False
+
 
 def gaussian_error_propagator(jac, stds=None, cov=None, rtol=1e-12, atol=1e-6):
     """
@@ -106,12 +177,12 @@ def gaussian_error_propagator(jac, stds=None, cov=None, rtol=1e-12, atol=1e-6):
         standard deviations of 'p'.
     cov (if not None):
         the covariance of 'p'. --> its diagonal is stds**2 .
-    
+
     --> At least one of 'stds' and 'cov' must be given.
-    
+
     Returns:
         the full covariance matrix of f(p) .
-    
+
     NOTEs:
     - The mean values of p; i.e. p_mean, do not matter!
     - The tolerances are meant for:
@@ -120,21 +191,25 @@ def gaussian_error_propagator(jac, stds=None, cov=None, rtol=1e-12, atol=1e-6):
     """
     if (stds is None) and (cov is None):
         raise ValueError(f"At least one of stds or cov must be given.")
-    else: # At least one of stds and/or cov is given.
-        if (stds is not None) and (cov is not None): # if both 'stds' 'cov' are given, we check them to agree.
-            e = np.linalg.norm(np.diag(cov)**(0.5) - stds) / np.linalg.norm(stds)
+    else:  # At least one of stds and/or cov is given.
+        if (stds is not None) and (
+            cov is not None
+        ):  # if both 'stds' 'cov' are given, we check them to agree.
+            e = np.linalg.norm(np.diag(cov) ** (0.5) - stds) / np.linalg.norm(stds)
             if e > rtol:
-                raise ValueError(f"The diagonal values of the 'cov' do not agree with the 'stds'.")
-        if cov is None: # no correlation among the inputs are considered.
-            assert len(stds)==jac.shape[1]
+                raise ValueError(
+                    f"The diagonal values of the 'cov' do not agree with the 'stds'."
+                )
+        if cov is None:  # no correlation among the inputs are considered.
+            assert len(stds) == jac.shape[1]
             return (jac @ np.diag(stds**2)) @ jac.T
             # NOTE: the diagonal entries will form: (jac**2) @ (np.array(stds)**2)
             # , however, the return value above has non-diagonal terms as well.
         else:
-            assert cov.shape[0]==cov.shape[1]==jac.shape[1]
+            assert cov.shape[0] == cov.shape[1] == jac.shape[1]
             if not np.allclose(cov, cov.T, rtol=rtol, atol=atol):
                 # covariance matrix must be symmetric for the following formula to be valid.
-                _e = np.linalg.norm(cov-cov.T) / np.linalg.norm(cov)
+                _e = np.linalg.norm(cov - cov.T) / np.linalg.norm(cov)
                 _msg = f"Covariance matrix must be symmetric (needed for Gaussian uncertainty propagation)."
                 _msg += f" Relative error = np.linalg.norm(cov-cov.T) / np.linalg.norm(cov) = {_e:.2e} ."
                 raise ValueError(_msg)
--- examples/VB_ASR/infer_constitutive_law_ASR_RCA.py
+++ examples/VB_ASR/infer_constitutive_law_ASR_RCA.py
@@ -1,29 +1,73 @@
 import sys
-if './examples/VB_ASR/' not in sys.path:
-    sys.path.append('./examples/VB_ASR/')
+
+if "./examples/VB_ASR/" not in sys.path:
+    sys.path.append("./examples/VB_ASR/")
 
 from helpers import *
 from bayem import vba, MVN, Gamma
 
-eps_0 = 0.
+eps_0 = 0.0
 X0 = [0.1825, 38.21, 167.9]
 return_jacobian = True
 _path = f"./examples/VB_ASR/results/"
 make_path(_path)
 
 ###############################
-    # DATA
+# DATA
 ###############################
 
-# ts_data = np.array([0, 8, 14, 19, 26, 46, 84, 123, 140, 173, 197, 200, 235, 252, 264, 284, 322, 361])
-# epss_data = [0, 0.01, 0.01, 0, 0, 0.01, 0.02, 0.01, 0.04, 0.08, 0.09, 0.09, 0.11, 0.13, 0.12, 0.13, 0.14, 0.13]
+# ts_data = np.array(
+#     [0, 8, 14, 19, 26, 46, 84, 123, 140, 173, 197, 200, 235, 252, 264, 284, 322, 361]
+# )
+# epss_data = [
+#     0,
+#     0.01,
+#     0.01,
+#     0,
+#     0,
+#     0.01,
+#     0.02,
+#     0.01,
+#     0.04,
+#     0.08,
+#     0.09,
+#     0.09,
+#     0.11,
+#     0.13,
+#     0.12,
+#     0.13,
+#     0.14,
+#     0.13,
+# ]
 
-ts_data = np.array([0, 8, 14, 19, 26, 46, 84, 123, 140, 173, 197, 200, 235, 252, 264, 284, 322, 361])
-epss_data = [0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.027, 0.059, 0.092, 0.126, 0.143, 0.155, 0.161, 0.172, 0.182, 0.185]
+ts_data = np.array(
+    [0, 8, 14, 19, 26, 46, 84, 123, 140, 173, 197, 200, 235, 252, 264, 284, 322, 361]
+)
+epss_data = [
+    0.000,
+    0.000,
+    0.000,
+    0.000,
+    0.000,
+    0.000,
+    0.000,
+    0.000,
+    0.027,
+    0.059,
+    0.092,
+    0.126,
+    0.143,
+    0.155,
+    0.161,
+    0.172,
+    0.182,
+    0.185,
+]
 
 ###############################
-    # MODEL
+# MODEL
 ###############################
+
 
 def eps_total(t, eps_inf=X0[0], Tau_c=X0[1], Tau_L=X0[2]):
     if return_jacobian:
@@ -33,21 +77,24 @@ def eps_total(t, eps_inf=X0[0], Tau_c=X0[1], Tau_L=X0[2]):
     else:
         z = zeta_func(t, Tau_c, Tau_L)
         return eps_0 + eps_inf * z
-    
+
+
 def zeta_func(t, Tau_c, Tau_L):
-    a1 = np.exp(- t / Tau_c)
+    a1 = np.exp(-t / Tau_c)
     a2 = np.exp((-t + Tau_L) / Tau_c)
     if return_jacobian:
-        d1 = - t * a1 / (Tau_c**2*(a2 + 1.))
-        d1 += (1. - a1) * (-t + Tau_L) * a2 / (Tau_c**2 * (a2 + 1.)**2)
-        d2 = - (1. - a1) * a2 / (Tau_c * ( a2 + 1.)**2)
-        return (1. - a1) / (1. + a2), np.array([d1, d2])
+        d1 = -t * a1 / (Tau_c**2 * (a2 + 1.0))
+        d1 += (1.0 - a1) * (-t + Tau_L) * a2 / (Tau_c**2 * (a2 + 1.0) ** 2)
+        d2 = -(1.0 - a1) * a2 / (Tau_c * (a2 + 1.0) ** 2)
+        return (1.0 - a1) / (1.0 + a2), np.array([d1, d2])
     else:
-        return (1. - a1) / (1. + a2)
+        return (1.0 - a1) / (1.0 + a2)
+
 
 ###############################
-    # OBJECTIVE (MODEL ERROR)
+# OBJECTIVE (MODEL ERROR)
 ###############################
+
 
 def me_vec(X):
     eps_inf, Tau_c, Tau_L = X
@@ -58,6 +105,7 @@ def me_vec(X):
         f = eps_total(ts_data, eps_inf=eps_inf, Tau_c=Tau_c, Tau_L=Tau_L)
         return np.array(f) - np.array(epss_data)
 
+
 def me_scalar(X):
     if return_jacobian:
         f, diff = me_vec(X)
@@ -66,12 +114,13 @@ def me_scalar(X):
         f = me_vec(X)
         return 0.5 * np.dot(f, f)
 
+
 ###############################
-    # VARIATIONAL BAYESIAN (VB)
+# VARIATIONAL BAYESIAN (VB)
 ###############################
 
-prior_mvn = MVN(mean=X0, precision=np.diag([(abs(x))**(-2) for x in X0]))
-n0 = Gamma(shape=1.5, scale=175000.)
+prior_mvn = MVN(mean=X0, precision=np.diag([(abs(x)) ** (-2) for x in X0]))
+n0 = Gamma(shape=1.5, scale=175000.0)
 # n0 = Gamma(shape=0.84, scale=15000.)
 # n0 = Gamma(shape=0.5, scale=500000.)
 # n0 = Gamma(shape=0.84, scale=370000.)
@@ -80,33 +129,53 @@ vb_results = vba(f=me_vec, x0=prior_mvn, noise0=n0, jac=return_jacobian)
 ### Plot free energy
 plt.figure()
 n_vb = len(vb_results.free_energies)
-xs = list(range(1, n_vb+1))
-plt.plot(xs, vb_results.free_energies, marker='.')
+xs = list(range(1, n_vb + 1))
+plt.plot(xs, vb_results.free_energies, marker=".")
 plt.title(f"Free energy", size=12)
-plt.xlabel('VB iteration', size=12)
-plt.ylabel('F', size=12)
+plt.xlabel("VB iteration", size=12)
+plt.ylabel("F", size=12)
 plt.xticks(xs)
-plt.savefig(f"{_path}Free_energies.png", bbox_inches='tight', dpi=300)
+plt.savefig(f"{_path}Free_energies.png", bbox_inches="tight", dpi=300)
 plt.show()
 
 ### Plot Parameters
 from scipy.stats import norm, gamma
-pars_names = ['eps_inf', 'Tau_c', 'Tau_L']
+
+pars_names = ["eps_inf", "Tau_c", "Tau_L"]
 pr_m = vb_results.param0.mean
 pr_std = vb_results.param0.std_diag
 post_m = vb_results.param.mean
 post_std = vb_results.param.std_diag
 noise0 = vb_results.noise0
-options_prior = {'linestyle':'', 'color':'orange', 'marker':'<', 'fillstyle':'full'}
-options_posterior = {'linestyle':'', 'color':'mediumblue', 'marker':'>', 'fillstyle':'full'}
-options_predict = {'linestyle':'', 'color':'purple', 'marker':'d', 'fillstyle':'full'}
+options_prior = {"linestyle": "", "color": "orange", "marker": "<", "fillstyle": "full"}
+options_posterior = {
+    "linestyle": "",
+    "color": "mediumblue",
+    "marker": ">",
+    "fillstyle": "full",
+}
+options_predict = {
+    "linestyle": "",
+    "color": "purple",
+    "marker": "d",
+    "fillstyle": "full",
+}
 for i, n in enumerate(pars_names):
-        pr_dist = norm(pr_m[i], pr_std[i])
-        post_dist = norm(post_m[i], post_std[i])
-        ## plot prior-posterior together
-        plot_pdfs([pr_dist, post_dist], labels=['Prior', 'Posterior'], colors=[options_prior['color'] \
-                , options_posterior['color']], tit=f"Parameter {n}" \
-                  , xl=n, _path=_path, _name=f"Parameter_{n}", _format='.png', dpi=300, sz=16)
+    pr_dist = norm(pr_m[i], pr_std[i])
+    post_dist = norm(post_m[i], post_std[i])
+    ## plot prior-posterior together
+    plot_pdfs(
+        [pr_dist, post_dist],
+        labels=["Prior", "Posterior"],
+        colors=[options_prior["color"], options_posterior["color"]],
+        tit=f"Parameter {n}",
+        xl=n,
+        _path=_path,
+        _name=f"Parameter_{n}",
+        _format=".png",
+        dpi=300,
+        sz=16,
+    )
 
 ### Plot noises
 interval_prob = 0.99
@@ -118,38 +187,55 @@ sh_post = n1.shape
 sc_post = n1.scale
 g_pr = gamma(a=sh_pr, scale=sc_pr)
 g_post = gamma(a=sh_post, scale=sc_post)
-plot_pdfs([g_pr, g_post], labels=['Prior', 'Posterior'], colors=[options_prior['color'] \
-        , options_posterior['color']], interval_prob=interval_prob \
-            , tit=f"Inference of precision of noise", xl="$\phi$", _path=_path \
-                , _name=f"Noise_precision", _format='.png', dpi=300, sz=16)
+plot_pdfs(
+    [g_pr, g_post],
+    labels=["Prior", "Posterior"],
+    colors=[options_prior["color"], options_posterior["color"]],
+    interval_prob=interval_prob,
+    tit=f"Inference of precision of noise",
+    xl="$\phi$",
+    _path=_path,
+    _name=f"Noise_precision",
+    _format=".png",
+    dpi=300,
+    sz=16,
+)
 
 
 if return_jacobian:
     # propagation of parameters distributions into the model
-    f_at_posterior, diff = eps_total(ts_data, eps_inf=post_m[0], Tau_c=post_m[1], Tau_L=post_m[2])
+    f_at_posterior, diff = eps_total(
+        ts_data, eps_inf=post_m[0], Tau_c=post_m[1], Tau_L=post_m[2]
+    )
     cov = vb_results.param.cov
-    cov_propagated = gaussian_error_propagator(jac=diff, stds=post_std \
-                                                , cov=cov, rtol=1e-12, atol=1e-5)
+    cov_propagated = gaussian_error_propagator(
+        jac=diff, stds=post_std, cov=cov, rtol=1e-12, atol=1e-5
+    )
     # uncertainty of the identified noise
     cov_inv = vb_results.cov_inv[0]
-    cov_at_noise_mean = 1. / vb_results.noise.mean * np.linalg.inv(cov_inv)
+    cov_at_noise_mean = 1.0 / vb_results.noise.mean * np.linalg.inv(cov_inv)
     # Sum
     predicted_std_at_post = np.diag(cov_propagated + cov_at_noise_mean) ** (0.5)
 else:
-    f_at_posterior = eps_total(ts_data, eps_inf=post_m[0], Tau_c=post_m[1], Tau_L=post_m[2])
-    print(f"Propagation of parameter variability into the model is ignored, since no jacobian is provided.")
+    f_at_posterior = eps_total(
+        ts_data, eps_inf=post_m[0], Tau_c=post_m[1], Tau_L=post_m[2]
+    )
+    print(
+        f"Propagation of parameter variability into the model is ignored, since no jacobian is provided."
+    )
 
 ###############################
-    # SCIPY OPTIMIZE
+# SCIPY OPTIMIZE
 ###############################
 
 import scipy.optimize as opt
-method = 'trust-constr'
+
+method = "trust-constr"
 opt_results = opt.minimize(me_scalar, x0=X0, method=method, jac=return_jacobian)
 print(opt_results)
 
 ###############################
-    # PLOT ALL RESULTs
+# PLOT ALL RESULTs
 ###############################
 
 if return_jacobian:
@@ -159,14 +245,42 @@ else:
     epss_0 = eps_total(ts_data)
     epss_opt = eps_total(ts_data, *opt_results.x)
 plt.figure()
-plt.plot(ts_data, epss_data, linestyle='', marker='*', label='data', color='red')
-plt.plot(ts_data, epss_0, linestyle='--', marker='.', label='model\n(initial guess)', color='gray')
-plt.plot(ts_data, epss_opt, linestyle='--', marker='.', label='model\n(scipy-optimized)', color='green')
-plt.plot(ts_data, f_at_posterior, linestyle='--', marker='o', fillstyle='none', label='model (mean)\n(VB-optimized)', color=options_predict['color'])
-plt.fill_between(ts_data, f_at_posterior-2*predicted_std_at_post, f_at_posterior+2*predicted_std_at_post \
-                          , label='model (4$\sigma$)\n(VB-optimized)', alpha=0.10, **{'color':options_predict['color']})
-plt.xlabel('Time')
-plt.ylabel('Expansion')
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig(f"{_path}fitted_model_lab_scale.png", bbox_inches='tight', dpi=400)
+plt.plot(ts_data, epss_data, linestyle="", marker="*", label="data", color="red")
+plt.plot(
+    ts_data,
+    epss_0,
+    linestyle="--",
+    marker=".",
+    label="model\n(initial guess)",
+    color="gray",
+)
+plt.plot(
+    ts_data,
+    epss_opt,
+    linestyle="--",
+    marker=".",
+    label="model\n(scipy-optimized)",
+    color="green",
+)
+plt.plot(
+    ts_data,
+    f_at_posterior,
+    linestyle="--",
+    marker="o",
+    fillstyle="none",
+    label="model (mean)\n(VB-optimized)",
+    color=options_predict["color"],
+)
+plt.fill_between(
+    ts_data,
+    f_at_posterior - 2 * predicted_std_at_post,
+    f_at_posterior + 2 * predicted_std_at_post,
+    label="model (4$\sigma$)\n(VB-optimized)",
+    alpha=0.10,
+    **{"color": options_predict["color"]},
+)
+plt.xlabel("Time")
+plt.ylabel("Expansion")
+plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+plt.savefig(f"{_path}fitted_model_lab_scale.png", bbox_inches="tight", dpi=400)
 plt.show()
